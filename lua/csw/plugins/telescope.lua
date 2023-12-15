328fb087b512625cd6a0414aa473febd3524dfe8
Shaun Garwood <shaun.garwood@gmail.com>
2023-12-15 16:16:02 -0700

    Remove conflicting keymappings

--- lua/csw/plugins/telescope.lua
+++ lua/csw/plugins/telescope.lua
@@ -12,14 +12,7 @@ return {
 
     telescope.setup({
       defaults = {
-        path_display = { "truncate " },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
+        path_display = { "truncate " }
       },
     })
 
