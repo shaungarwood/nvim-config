8b9be60488533103cbb29dbb0257ccfe04d41fda
Shaun Garwood <shaun.garwood@gmail.com>
2024-02-09 15:05:34 -0700

    working switch to lazyvim, some bugs

--- lua/csw/lazy.lua
+++ lua/csw/lazy.lua
@@ -9,18 +9,50 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 -- require("lazy").setup({ { import = "csw.plugins" }, { import = "csw.plugins.lsp" } }, {
-require("lazy").setup({ { import = "csw.plugins" } }, {
-  install = {
-    colorscheme = { "nightfly" },
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "csw.plugins" }
   },
-  checker = {
-    enabled = true,
-    notify = false,
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  change_detection = {
-    notify = false,
+  install = { colorscheme = { "nightfly" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
   },
 })
+--   {
+--   install = {
+--     colorscheme = { "nightfly" },
+--   },
+--   checker = {
+--     enabled = true,
+--     notify = false,
+--   },
+--   change_detection = {
+--     notify = false,
+--   },
+-- })
