4a5b1dc46c9f7ea3818b5506fff53cf309903a16
Shaun Garwood <shaun.garwood@gmail.com>
2024-03-25 13:29:24 -0600

    copilot settings

--- lua/csw/plugins/copilot-chat.lua
+++ lua/csw/plugins/copilot-chat.lua
@@ -6,26 +6,17 @@ return {
       debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
       disable_extra_info = 'no', -- Disable extra information (e.g: system prompt) in the response.
       -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
+      prompts = {
+        Explain = "Briefly explain how it works, please.",
+        Tests = "Briefly explain how the selected code works, then generate unit tests in rspec, please."
+      }
     },
     build = function()
       vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
     end,
     event = "VeryLazy",
     keys = {
-      { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-      { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
-      {
-        "<leader>ccv",
-        ":CopilotChatVisual",
-        mode = "x",
-        desc = "CopilotChat - Open in vertical split",
-      },
-      {
-        "<leader>ccx",
-        ":CopilotChatInPlace<cr>",
-        mode = "x",
-        desc = "CopilotChat - Run in-place code",
-      },
+      { "<leader>cc", ":CopilotChat ", desc = "CopilotChat - chat" },
     },
   },
 }
